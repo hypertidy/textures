012ea9a482dd8cb3d3b8e40a0eba40acb3c98def
Michael Sumner <mdsumner@gmail.com>
2020-05-21 15:26:48 +1000

    begin story outline

---
+++ rough-examples.R
@@ -0,0 +1,50 @@
+library(textures)
+library(rgl)
+
+# 1. absolutely simplest native coordinate texture
+# -----------------------------------------------
+suppressWarnings(quad0 <- anglr::as.mesh3d(matrix(0, 1))) ## to be fixed
+quad0$texcoords <- quad0$vb[1:2, ]
+tfile <- tempfile()
+png::writePNG(ga_topo$img/255, tfile)
+quad0$material$texture <- tfile
+
+quad0$vb[1L,] <- scales::rescale(quad0$vb[1,], to = ga_topo$extent[c("xmin", "xmax")])
+quad0$vb[2L,] <- scales::rescale(quad0$vb[2L,], to = ga_topo$extent[c("ymin", "ymax")])
+
+
+rgl::open3d()
+rgl::par3d(windowRect = c(0, 0, 1024, 1024))
+rgl::plot3d(quad0, specular = "black"); rgl::view3d(phi = 0, interactive = FALSE)
+
+
+# 2. reproject the canvas from the single quad above
+# ---------------------------------------------------
+## now do the same but we reproject x,y to something non-native for the image
+## (it won't work very well)
+
+quad0$vb[1:2, ] <- t(reproj::reproj(t(quad0$vb[1:2, ]), target = "+proj=laea +lon_0=120 +lat_0=-28",
+                                 source = ga_topo$crs)[,1:2])
+
+## it *looks ok* but if we overplot you'll see the problem
+rgl::plot3d(quad0, specular = "black"); rgl::view3d(phi = 0, interactive = FALSE)
+
+
+# 3. densify the canvas prior to reprojection (obviously)
+# -------------------------------------------------------
+## now let's subdivide the canvas
+suppressWarnings(quad0 <- anglr::as.mesh3d(matrix(0, 1))) ## to be fixed
+plot(t(quad0$vb[1:2, ]), pch = ".")
+quad0$material$color
+quad0 <- subdivision3d(quad0, depth =6, normalize = TRUE, deform = FALSE)
+plot(t(quad0$vb[1:2, ]), pch = ".")
+
+quad0$texcoords <- quad0$vb[1:2, ]
+quad0$material$texture <- tfile
+
+quad0$vb[1L,] <- scales::rescale(quad0$vb[1,], to = ga_topo$extent[c("xmin", "xmax")])
+quad0$vb[2L,] <- scales::rescale(quad0$vb[2L,], to = ga_topo$extent[c("ymin", "ymax")])
+quad0$vb[1:2, ] <- t(reproj::reproj(t(quad0$vb[1:2, ]), target = "+proj=laea +lon_0=20 +lat_0=-28",
+                                    source = ga_topo$crs)[,1:2])
+
+rgl::plot3d(quad0, specular = "black"); rgl::view3d(phi = 0, interactive = FALSE)
